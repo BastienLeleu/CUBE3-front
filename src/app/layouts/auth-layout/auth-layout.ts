d43103fc23aa5d62c0cfb783c38ff7670dda680d
Bastien Leleu <bastienleleu6@gmail.com>
2026-07-03 19:49:12 +0200

    feat(footer): add modern footer with interactive legal notices and terms of use modals

--- src/app/layouts/auth-layout/auth-layout.ts
+++ src/app/layouts/auth-layout/auth-layout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './auth-layout.html',
   styleUrl: './auth-layout.scss',
 })
--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../shared/components/header/header';
+import { FooterComponent } from '../../shared/components/footer/footer';
 import { CartComponent } from '../../cart/cart/cart';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, HeaderComponent, CartComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CartComponent],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss'
 })
